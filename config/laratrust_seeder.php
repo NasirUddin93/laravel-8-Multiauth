e6c0caf7f1f920c6ebac42c73edcd56d8071beac
NasirUddin93 <nasir93cse@gmail.com>
2021-05-01 23:04:49 +0600

    Multiauth is done by using Laravel-8

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,33 @@
+<?php
+
+
+return [
+    'create_user' => false,
+    'truncate_tables' => true,
+    'roles_structure' => [
+        'admin' => [
+            'users' => 'c,r,u,d',
+            'payments' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ],
+        'blogwriter' => [
+            'users' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ],
+        'user' => [
+            'profile' => 'r,u',
+        ]  
+        // 'role_name' => [
+        //     'module_1_name' => 'c,r,u,d',
+        // ]
+    ],
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete'
+    ],
+    
+];
+
+?>
